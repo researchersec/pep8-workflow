c1ab0c652f5ced6adcfcd85c47555f72e93c7667
researchersec <109282836+researchersec@users.noreply.github.com>
2023-09-17 00:20:54 +0200

    Update books-pep8.py

--- books-pep8.py
+++ books-pep8.py
@@ -1,45 +1,42 @@
-"""Module for demonstrating a simple Book class."""
+"""This module contains the Literature class and its related operations."""
 
-
-class Book:
-    """A simple Book class to demonstrate OOP concepts."""
-
-    def __init__(self, title, author, pages, price):
-        """Initialize the attributes of the book."""
+class Literature:
+    """A class to represent a piece of literature."""
+    
+    def __init__(self, title, writer, length, cost):
+        """Initialize the literature's attributes."""
         self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
-        self.is_sold = False
+        self.writer = writer
+        self.length = length
+        self.cost = cost
+        self.is_purchased = False
 
-    def sell(self):
-        """Mark the book as sold."""
-        if not self.is_sold:
-            self.is_sold = True
-            print(f"The book '{self.title}' is now sold.")
+    def purchase(self):
+        """Mark the literature as purchased and print an appropriate message."""
+        if not self.is_purchased:
+            self.is_purchased = True
+            print(f"The literature '{self.title}' is now purchased.")
         else:
-            print(f"The book '{self.title}' is already sold.")
+            print(f"The literature '{self.title}' is already purchased.")
 
-    def display(self):
-        """Display the details of the book."""
+    def showcase(self):
+        """Display the literature's details."""
         print(f"Title: {self.title}")
-        print(f"Author: {self.author}")
-        print(f"Pages: {self.pages}")
-        print(f"Price: ${self.price:.2f}")
-        print(f"Is Sold: {self.is_sold}")
+        print(f"Writer: {self.writer}")
+        print(f"Length: {self.length}")
+        print(f"Cost: ${self.cost:.2f}")
+        print(f"Is Purchased: {self.is_purchased}")
 
+def execute():
+    """Main function to create and interact with Literature instances."""
+    literature1 = Literature("Python Lore", "John Deer", 320, 28.99)
+    literature2 = Literature("Mastering Python Tales", "Jane Snow", 520, 47.99)
+    literature3 = Literature("Python in Mythology", "Alice Green", 420, 38.99)
 
-def main():
-    """Create and interact with Book instances."""
-    book1 = Book("Python for Beginners", "John Doe", 300, 29.99)
-    book2 = Book("Advanced Python", "Jane Smith", 500, 49.99)
-    book3 = Book("Data Science with Python", "Alice Brown", 400, 39.99)
-
-    book1.display()
-    book2.sell()
-    book2.display()
-    book3.display()
-
+    literature1.showcase()
+    literature2.purchase()
+    literature2.showcase()
+    literature3.showcase()
 
 if __name__ == "__main__":
-    main()
+    execute()
